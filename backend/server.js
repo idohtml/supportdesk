70abc8348dc0eaa957a8378b8f1309a627f1936f
idohtml <jacques.kanu@gmail.com>
2023-11-02 08:02:29 +0100

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const errorHandler = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const PORT = process.env.PORT || 5000;
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to the supportdesk API" });
+});
+
+// Routes
+app.use("/api/users", require("./routes/userRoutes"));
+
+// Error Middleware
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
